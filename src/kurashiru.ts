4c407cd1d30b4ab169a8724e8eeaf66306e032bc
agumy <nov13sh@gmail.com>
2021-06-03 22:57:48 +0900

    build data structure

--- src/kurashiru.ts
+++ src/kurashiru.ts
@@ -1,5 +1,5 @@
 import https from "https";
-import { JSDOM } from "jsdom";
+import jsdom from "jsdom";
 
 const url = process.argv[2];
 
@@ -9,12 +9,27 @@ https.get(url, (res) => {
   let html = "";
   res.on("data", (line) => (html += line));
   res.on("end", () => {
-    const dom = new JSDOM(html);
+    const dom = new jsdom.JSDOM(html);
     const { document } = dom.window;
-    const h1 = document.querySelector("h1");
-    const ingredientList = document.querySelector(".ingredient-list");
-    console.log(h1?.textContent);
+    const title = document
+      .querySelector("h1")
+      ?.textContent?.replace("レシピ・作り方", "")
+      .trim();
 
-    console.dir(ingredientList?.textContent);
+    console.log({ title });
+
+    const ingredientList = Array.from(
+      document.querySelector(".ingredient-list")?.children || []
+    );
+
+    const jsonIngredientList = ingredientList.map((e) => {
+      const ingredient = e.children;
+      return {
+        name: ingredient[0].textContent?.trim(),
+        amount: ingredient[1].textContent?.trim(),
+      };
+    });
+
+    console.log(jsonIngredientList);
   });
 });
